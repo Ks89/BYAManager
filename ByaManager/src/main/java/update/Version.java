cd2f60c8a973b35867d79a100a8755fd67e68756
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-09-17 22:44:21 +0200

    1.0.1

--- ByaManager/src/main/java/update/Version.java
+++ ByaManager/src/main/java/update/Version.java
@@ -20,7 +20,7 @@ import notification.Notification;
 
 public final class Version {
 
-	public static final String VERSION = "1,0,0,0";
+	public static final String VERSION = "1,0,1,0";
 	
 	private static int majorSw;
 	private static int minorSw;
