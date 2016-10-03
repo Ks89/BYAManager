8d7e6948535431aff37440b57a7b5c3ff3410032
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-10-03 20:26:20 +0200

    moved to Intellij Idea 2016

---
+++ src/main/java/connection/TestConnection.java
@@ -0,0 +1,56 @@
+/*
+Copyright 2011-2015 Stefano Cappa
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+package connection;
+
+import notification.Notification;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+
+/**
+ *	Classe che si occupa di testare la connessione internet 
+ *	prima di avviare il programma.
+ */
+public class TestConnection {
+	private static final Logger LOGGER = LogManager.getLogger(TestConnection.class);
+
+	/**
+	 * Verifica la connettivita tramite un get http.
+	 * Non lo fa con il ping perche' si puo' anche disabilitare.
+	 * @throws IOException
+	 */
+	public void testConnection() throws IOException {
+		LOGGER.info("verificaRete() - Avviata verifica rete");
+
+		URI uri = URI.create("http://www.google.com/");
+		HttpURLConnection connection = (HttpURLConnection)uri.toURL().openConnection();
+		connection.connect();
+
+		if (connection.getResponseCode() / 100 != 2) {
+			Notification.showErrorOptionPane("download201", "Server error 201");
+			return;
+		}
+
+		connection.getContent();
+		LOGGER.info("verificaRete() - Eseguita richiesta verifica rete " + uri);
+	}
+}
---
+++ src/main/java/fileutility/FilesMerger.java
@@ -0,0 +1,120 @@
+/*
+Copyright 2011-2015 Stefano Cappa
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package fileutility;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Classe che si occupa di unire i file .part in un solo .ipsw 
+ * verificando che le parti siano concatenate nella giusta sequenza 
+ * e a cancellare i .part che ora non servono piu'
+ */
+public class FilesMerger {
+	private static final Logger LOGGER = LogManager.getLogger(FilesMerger.class);
+	private Path downloadTempPath;
+	private Path shaFiledownloadPath;
+	private String nomeFile;
+
+	/**
+	 * Metodo per unire le parti del download.
+	 * @param nomeFile String che rappresenta il nome del file associato al Download.
+	 */
+	public FilesMerger(Path downloadPath, String nomeFile) {
+		this.nomeFile = nomeFile;
+		this.downloadTempPath = downloadPath.resolve("temp");
+		this.shaFiledownloadPath = downloadPath.resolve(nomeFile + ".sha");
+	}
+
+	/**
+	 * Metodo che si occupa di concatenare le parti, richiamato da unisciParti.
+	 * @param array di File che rappresenta la lista dei file ordinata per sequenza.
+	 * @throws IOException
+	 */
+	private void mergeParts(Path[] pathPartsList) {
+		FileChannel readChannel;
+		long byteTransferred;
+
+		try (
+				FileChannel writeChannel = FileChannel.open(shaFiledownloadPath,StandardOpenOption.WRITE,StandardOpenOption.CREATE)
+				) {
+			for(Path pathPart : pathPartsList) {	
+				if(pathPart!=null) {
+					readChannel = FileChannel.open(pathPart,StandardOpenOption.READ);
+					byteTransferred = readChannel.transferTo(0, Files.size(pathPart), writeChannel);
+
+					readChannel.close();
+
+					if(byteTransferred!=Files.size(pathPart)) {
+						throw new IOException("Byte da trasferire: " + Files.size(pathPart) + " , trasferiti: " + byteTransferred);
+					}
+				}
+			}
+		} catch (IOException e) {
+			LOGGER.error("concatenaParti() - IOException= " + e);
+		}
+	}
+
+	/**
+	 * Metodo che si occupa di creare l'ipsw verficando l'ordine della sequenza di concatenazione delle parti,
+	 * per poi chiamare concatenaParti() e per finire il rimuoviParti() che si occupa di cancellare i file
+	 * che non servono piu' a nulla, cioe' tutti i .part.
+	 */
+	public void createCompleteIpsw() {
+		Path[] pathPartsList = new Path[4];
+		List<Path> pathList = FileList.getFileList(downloadTempPath); //lista delle parti cosi' come vengono rilevate, senza ordine preciso
+
+		int temp; //variabile usata per memorizzare il numero del file .part in modo temporaneo
+
+		for(Path pathPart: pathList) {
+			if(pathPart!=null && pathPart.endsWith(nomeFile+".part"+pathPart.toString().charAt(pathPart.toString().length()-1)) 
+					&& pathPart.getFileName().toString().contains(".part")) {					
+				temp = Integer.parseInt(pathPart.getFileName().toString().replace(nomeFile + ".part", ""));
+				pathPartsList[temp-1] = pathPart;
+			}	
+		}
+
+		//qui ho ottenuto la lista ordinata delle parti, cosi' la passo al concatenatore delle parti
+		this.mergeParts(pathPartsList);
+	}
+
+	/**
+	 * Metodo che si occupa di rimuovere i file nella cartella dei
+	 * download temporanei quando il nome del file contiene quello passato come
+	 * parametro durante la creazione di questa classe.
+	 */
+	public void removeParts() {
+		//cancella le parti del file scaricato
+		List<Path> fileList = FileList.getFileList(downloadTempPath);
+		for(Path partPath : fileList) {
+			if(partPath!=null && partPath.endsWith(nomeFile)) {
+				try {
+					Files.deleteIfExists(partPath);
+				} catch (IOException e) {
+					LOGGER.error("Errore durante la rimozione di: " + partPath.toString());
+				}
+			}
+		}
+	}
+}
